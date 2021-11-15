8fd0a774a0aacfe34adf26b8557dc5ce7570042a
Jack Baron <jackmbaron@gmail.com>
2021-11-15 07:30:22 +0000

    update flatbuffer schema

--- src/main/java/com/worldql/client/Messages/Instruction.java
+++ src/main/java/com/worldql/client/Messages/Instruction.java
@@ -6,15 +6,17 @@ public final class Instruction {
   private Instruction() { }
   public static final int Heartbeat = 0;
   public static final int Handshake = 1;
-  public static final int LocalMessage = 2;
-  public static final int GlobalMessage = 3;
-  public static final int RecordCreate = 4;
-  public static final int RecordRead = 5;
-  public static final int RecordUpdate = 6;
-  public static final int RecordDelete = 7;
-  public static final int RecordReply = 8;
-  public static final int AreaSubscribe = 9;
-  public static final int AreaUnsubscribe = 10;
+  public static final int PeerConnect = 2;
+  public static final int PeerDisconnect = 3;
+  public static final int AreaSubscribe = 4;
+  public static final int AreaUnsubscribe = 5;
+  public static final int GlobalMessage = 6;
+  public static final int LocalMessage = 7;
+  public static final int RecordCreate = 8;
+  public static final int RecordRead = 9;
+  public static final int RecordUpdate = 10;
+  public static final int RecordDelete = 11;
+  public static final int RecordReply = 12;
   public static final int Unknown = 255;
 }
 
