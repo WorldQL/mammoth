4bf364a72d8cf05e6c211bedc41bde24575077f3
Jack Baron <jackmbaron@gmail.com>
2021-12-02 08:27:40 +0000

    WIP: implement tile and drop handling doesn't work because the record flexbuffer is getting mangled somewhere

--- src/main/java/com/worldql/client/listeners/utils/BlockTools.java
+++ src/main/java/com/worldql/client/listeners/utils/BlockTools.java
@@ -1,15 +1,22 @@
 package com.worldql.client.listeners.utils;
 
+import com.google.flatbuffers.FlexBuffers;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.worldql.client.WorldQLClient;
 import com.worldql.client.listeners.PlayerBreakBlockListener;
 import com.worldql.client.serialization.Record;
 import com.worldql.client.serialization.Vec3D;
+import net.minecraft.core.BlockPosition;
+import net.minecraft.nbt.MojangsonParser;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.level.block.entity.TileEntity;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.craftbukkit.v1_18_R1.CraftWorld;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -29,19 +36,45 @@ public class BlockTools {
                     BlockData bd = Bukkit.createBlockData(record.data());
                     b.setBlockData(bd);
 
-                    if (record.flex() != null && isSelf && PlayerBreakBlockListener.pendingDrops.contains(record.uuid())) {
-                        PlayerBreakBlockListener.pendingDrops.remove(record.uuid());
+                    if (record.flex() != null) {
+                        FlexBuffers.Map map = FlexBuffers.getRoot(record.flex()).asMap();
 
-                        ItemStack[] drops = new ItemStack[0];
-                        try {
-                            drops = PlayerBreakBlockListener.deserializeItemStack(record.flex());
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                        // TODO: Remove debug logging
+                        WorldQLClient.getPluginInstance().getLogger().info("map = " + map.keys().toString());
+
+                        // Handle drops
+                        if (!map.get("drops").isNull() && isSelf && PlayerBreakBlockListener.pendingDrops.contains(record.uuid())) {
+                            PlayerBreakBlockListener.pendingDrops.remove(record.uuid());
+
+                            ItemStack[] drops = new ItemStack[0];
+                            try {
+                                drops = PlayerBreakBlockListener.deserializeItemStack(map.get("drops").asBlob().data());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            Location blockCenter = b.getLocation().add(0.5, 0.5, 0.5);
+                            for (ItemStack item : drops) {
+                                b.getWorld().dropItem(blockCenter, item);
+                            }
                         }
 
-                        Location blockCenter = b.getLocation().add(0.5, 0.5, 0.5);
-                        for (ItemStack item : drops) {
-                            b.getWorld().dropItem(blockCenter, item);
+                        if (!map.get("isTile").isNull() && map.get("isTile").asBoolean()) {
+                            var nbtString = map.get("nbt").asString();
+                            NBTTagCompound copied = null;
+                            try {
+                                copied = MojangsonParser.a(nbtString);
+                            } catch (CommandSyntaxException ex) {
+                                ex.printStackTrace();
+                            }
+
+                            Location l = b.getLocation();
+                            CraftWorld cw = (CraftWorld) l.getWorld();
+                            TileEntity t = cw.getHandle().getBlockEntity(new BlockPosition(l.getBlockX(), l.getBlockY(), l.getBlockZ()), true);
+
+                            if (copied != null && t != null) {
+                                t.a(copied);
+                            }
                         }
                     }
                 }
