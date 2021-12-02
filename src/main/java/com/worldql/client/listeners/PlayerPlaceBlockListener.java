dbd45a2ee195901faf742e1edffed16fe4ee5d26
Jack Baron <jackmbaron@gmail.com>
2021-12-02 08:25:53 +0000

    add nbt data to tile entity record as flex

--- src/main/java/com/worldql/client/listeners/PlayerPlaceBlockListener.java
+++ src/main/java/com/worldql/client/listeners/PlayerPlaceBlockListener.java
@@ -1,18 +1,24 @@
 package com.worldql.client.listeners;
 
+import com.google.flatbuffers.FlexBuffersBuilder;
 import com.worldql.client.WorldQLClient;
 import com.worldql.client.events.PlayerHoldEvent;
-import com.worldql.client.serialization.*;
 import com.worldql.client.serialization.Record;
+import com.worldql.client.serialization.*;
+import net.minecraft.core.BlockPosition;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.level.block.entity.TileEntity;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
+import org.bukkit.craftbukkit.v1_18_R1.CraftWorld;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import zmq.ZMQ;
 
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
@@ -21,12 +27,32 @@ public class PlayerPlaceBlockListener implements Listener {
 
     @EventHandler
     public void onPlayerPlaceBlockEvent(BlockPlaceEvent e) {
+        FlexBuffersBuilder b = Codec.getFlexBuilder();
+        int pmap = b.startMap();
+
+
+        Location l = e.getBlockPlaced().getLocation();
+        CraftWorld cw = (CraftWorld) e.getBlockPlaced().getLocation().getWorld();
+        TileEntity t = cw.getHandle().getBlockEntity(new BlockPosition(l.getBlockX(), l.getBlockY(), l.getBlockZ()), true);
+
+        // Save NBT data to flexbuffer
+        if (t != null) {
+            NBTTagCompound nbt = t.n();
+            b.putBoolean("isTile", true);
+            b.putString("nbt", nbt.toString());
+        } else {
+            b.putBoolean("isTile", false);
+        }
+
+        b.endMap(null, pmap);
+        ByteBuffer bb = b.finish();
+
         Record placedBlock = new Record(
                 UUID.nameUUIDFromBytes(e.getBlockPlaced().getLocation().toString().getBytes(StandardCharsets.UTF_8)),
                 new Vec3D(e.getBlockPlaced().getLocation()),
                 e.getBlockPlaced().getWorld().getName(),
                 e.getBlockPlaced().getBlockData().getAsString(),
-                null
+                bb
         );
         // TODO: Handle compound blocks (beds, doors) and joined blocks (fences, glass panes)
 
