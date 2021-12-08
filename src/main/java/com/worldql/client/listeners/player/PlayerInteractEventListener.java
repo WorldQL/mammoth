315f2d39331d32a0aa5bdd7c13f736da5d23784b
Jaxkr <jacksonroberts25@gmail.com>
2021-12-07 23:40:00 -0700

    detection of placed ender crystal.

--- src/main/java/com/worldql/client/listeners/player/PlayerInteractEventListener.java
+++ src/main/java/com/worldql/client/listeners/player/PlayerInteractEventListener.java
@@ -13,13 +13,22 @@ import net.minecraft.core.BlockPosition;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.level.block.entity.TileEntity;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.EnderCrystal;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import zmq.ZMQ;
 
 import java.nio.ByteBuffer;
+import java.util.List;
 
 public class PlayerInteractEventListener implements Listener {
     @EventHandler
@@ -40,12 +49,31 @@ public class PlayerInteractEventListener implements Listener {
             }
             return;
         }
-
          */
 
+        // This is apparently the most elegant way to do this...
+        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)
+                && e.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.END_CRYSTAL)
+                && e.getClickedBlock().getType().equals(Material.OBSIDIAN)) {
+            // Thanks to https://www.spigotmc.org/threads/trying-to-detect-when-end-crystals-spawn-by-player.375673/
+            (new BukkitRunnable() {
+                @Override
+                public void run() {
+                    List<Entity> entities = e.getPlayer().getNearbyEntities(4, 4, 4);
+                    for (Entity entity : entities) {
+                        if (EntityType.ENDER_CRYSTAL == entity.getType()) {
+                            EnderCrystal crystal = (EnderCrystal) entity;
+                            Block belowCrystal = crystal.getLocation().getBlock().getRelative(BlockFace.DOWN);
+                            if (e.getClickedBlock().equals(belowCrystal)) {
+                                System.out.println("WE PLACED AN END CRYSTAL");
+                            }
+                        }
+                    }
+                }
+            }).runTask(WorldQLClient.getPluginInstance());
 
-        if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
-            return;
+        }
+
         Location playerLocation = e.getPlayer().getLocation();
         String action = "punch";
         OutgoingMinecraftPlayerSingleAction.sendPacket(playerLocation, e.getPlayer(), action);
