2e683ef7208df193c12a2dcedbf5bd36eef59d2c
Jack Baron <jackmbaron@gmail.com>
2021-12-09 12:52:46 +0000

    only request block updates after last update

--- src/main/java/com/worldql/client/listeners/chunks/ChunkLoadEventListener.java
+++ src/main/java/com/worldql/client/listeners/chunks/ChunkLoadEventListener.java
@@ -3,6 +3,7 @@ package com.worldql.client.listeners.chunks;
 import com.worldql.client.WorldQLClient;
 import com.worldql.client.serialization.Instruction;
 import com.worldql.client.serialization.Message;
+import com.worldql.client.serialization.Replication;
 import com.worldql.client.serialization.Vec3D;
 import org.bukkit.Chunk;
 import org.bukkit.event.EventHandler;
@@ -10,7 +11,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 import zmq.ZMQ;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChunkLoadEventListener implements Listener {
+    private static final Map<Chunk, Long> seenChunks = Collections.synchronizedMap(new HashMap<>());
 
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent e) {
@@ -24,14 +30,26 @@ public class ChunkLoadEventListener implements Listener {
         int max_height = chunk.getWorld().getMaxHeight();
         int middle = (min_height + max_height) / 2;
 
+        String parameter = null;
+        if (seenChunks.containsKey(chunk)) {
+            long ts = seenChunks.get(chunk);
+            parameter = Long.toString(ts);
+        }
+
         Message recordMessage = new Message(
                 Instruction.RecordRead,
                 WorldQLClient.worldQLClientId,
                 chunk.getWorld().getName(),
-                new Vec3D(x, middle, z)
+                Replication.ExceptSelf,
+                new Vec3D(x, middle, z),
+                null,
+                null,
+                parameter,
+                null
         );
 
         WorldQLClient.getPluginInstance().getPushSocket().send(recordMessage.encode(), ZMQ.ZMQ_DONTWAIT);
+        seenChunks.put(chunk, System.currentTimeMillis());
 
         for (int i = min_height; i <= max_height; i += 16) {
             Vec3D position = new Vec3D(x, i, z);
