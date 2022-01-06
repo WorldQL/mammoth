360f33088cb8c3f468ee4d2d2e354c7fa29f7230
Jackson Roberts <jacksonroberts25@gmail.com>
2022-01-06 14:39:56 -0700

    Prevent dropping items too close to borders.

--- src/main/java/com/worldql/client/listeners/player/PlayerDropItemListener.java
+++ src/main/java/com/worldql/client/listeners/player/PlayerDropItemListener.java
@@ -1,6 +1,8 @@
 package com.worldql.client.listeners.player;
 
+import com.worldql.client.Slices;
 import com.worldql.client.WorldQLClient;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -10,6 +12,12 @@ public class PlayerDropItemListener implements Listener {
     public void onPlayerDropEvent(PlayerDropItemEvent e) {
         if (!WorldQLClient.getPluginInstance().playerDataSavingManager.isSafe(e.getPlayer())) {
             e.setCancelled(true);
+            return;
+        }
+        if (Slices.getDistanceFromDMZ(e.getPlayer().getLocation()) < 5) {
+            e.setCancelled(true);
+            e.getPlayer().sendMessage(ChatColor.GOLD + "You don't want to drop items on the ground here!" +
+                    ChatColor.BOLD + " Move further from a server border.");
         }
     }
 }
